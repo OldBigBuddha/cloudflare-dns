4cec348d761c60b38d6682bdb5599f7eafde36a7
OldBigBuddha <13755428+OldBigBuddha@users.noreply.github.com>
2024-03-30 20:03:32 +0900

    chore: apply fmt

--- stete.tf
+++ stete.tf
@@ -1,6 +1,6 @@
 terraform {
- backend "gcs" {
-   bucket  = "oldbigbuddha-cloudflare-dns-tfstate"
-   prefix  = "terraform/state"
- }
+  backend "gcs" {
+    bucket = "oldbigbuddha-cloudflare-dns-tfstate"
+    prefix = "terraform/state"
+  }
 }
